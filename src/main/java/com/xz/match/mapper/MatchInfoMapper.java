83a3a71de6e0e91bdb019c5c5790e73765adab5d
chenwf <linfan0529@gmail.com>
2020-11-19 16:28:00 +0800

    9995

--- src/main/java/com/xz/match/mapper/MatchInfoMapper.java
+++ src/main/java/com/xz/match/mapper/MatchInfoMapper.java
@@ -7,29 +7,65 @@ import org.apache.ibatis.annotations.Param;
 
 /**
  * @author chenwf
- * @date 2020/11/16
+ * @date 2020/11/19
  */
 public interface MatchInfoMapper {
     long countByExample(MatchInfoExample example);
 
     int deleteByExample(MatchInfoExample example);
 
-    int deleteByPrimaryKey(String id);
+    /**
+     * delete by primary key
+     *
+     * @param id primaryKey
+     * @return deleteCount
+     */
+    int deleteByPrimaryKey(Long id);
 
+    /**
+     * insert record to table
+     *
+     * @param record the record
+     * @return insert count
+     */
     int insert(MatchInfo record);
 
+    /**
+     * insert record to table selective
+     *
+     * @param record the record
+     * @return insert count
+     */
     int insertSelective(MatchInfo record);
 
     List<MatchInfo> selectByExample(MatchInfoExample example);
 
-    MatchInfo selectByPrimaryKey(String id);
+    /**
+     * select by primary key
+     *
+     * @param id primary key
+     * @return object by primary key
+     */
+    MatchInfo selectByPrimaryKey(Long id);
 
     int updateByExampleSelective(@Param("record") MatchInfo record, @Param("example") MatchInfoExample example);
 
     int updateByExample(@Param("record") MatchInfo record, @Param("example") MatchInfoExample example);
 
+    /**
+     * update record selective
+     *
+     * @param record the updated record
+     * @return update count
+     */
     int updateByPrimaryKeySelective(MatchInfo record);
 
+    /**
+     * update record
+     *
+     * @param record the updated record
+     * @return update count
+     */
     int updateByPrimaryKey(MatchInfo record);
 
     int updateBatch(List<MatchInfo> list);

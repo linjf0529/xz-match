abb20645a27cc13541b3a3a55d2671bcd90ac45a
chenwf <linfan0529@gmail.com>
2020-11-30 11:03:58 +0800

    选手端二维码修改成条形码

--- src/main/java/com/xz/match/utils/CodeUtils.java
+++ src/main/java/com/xz/match/utils/CodeUtils.java
@@ -6,13 +6,13 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import org.krysalis.barcode4j.impl.code128.Code128Bean;
+import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
 
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.Hashtable;
 
 /**
@@ -59,5 +59,32 @@ public class CodeUtils {
             }
         }
         return image;
+    }
+
+
+    /**
+     *具体实现
+     * @param msg
+     */
+    public static void getBarCode(String msg,HttpServletResponse response){
+        try {
+            //选择条形码类型(好多类型可供选择)
+            Code128Bean bean=new Code128Bean();
+            //设置长宽
+            final double moduleWidth=0.20;
+            final int resolution=350;
+            bean.setModuleWidth(moduleWidth);
+            bean.doQuietZone(false);
+            String format = "image/png";
+            // 输出流
+            BitmapCanvasProvider canvas = new BitmapCanvasProvider(response.getOutputStream(), format,
+                    resolution, BufferedImage.TYPE_INT_RGB, false, 0);
+            //生成条码
+            bean.generateBarcode(canvas,msg);
+            canvas.finish();
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
